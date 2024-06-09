131141b75446df856d75a815cff6d3bd6d795f02
Arnaud Mouttapa <arnaudmouttapa@gmail.com>
2024-06-09 17:44:38 +0200

    add true data

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
 import streamlit as st
 import pandas as pd
 import duckdb
+import io
 
-st.write("""
-# SQL SRS
-Spaced Repetition System SQL practice
-""")
+csv = """
+beverage,price
+orange juice,2.5
+Expresso,2
+Tea,3
+"""
+
+beverages = pd.read_csv(io.StringIO(csv))
+
+csv2 = """
+food_item,food_price
+cookie juice,2.5
+chocolatine,2
+muffin,3
+"""
+
+food_items = pd.read_csv(io.StringIO(csv2))
+
+answer = """
+SELECT * FROM beverages
+CROSS JOIN food_items
+"""
+
+solution = duckdb.sql(answer).df()
+
+tab1, tab2 = st.tabs(['Tables', 'Solution'])
 
 option = st.selectbox(
     "What would you like to review?"
@@ -16,23 +39,31 @@ option = st.selectbox(
 
 st.write('You selected:', option)
 
-query = st.text_input('Enter your sql query')
+with tab1:
+    st.write("""
+    # SQL SRS
+    Spaced Repetition System SQL practice
+    """)
 
-df = pd.DataFrame({
-    'col1': [1, 2, 3, 4],
-    'col2': [5, 6, 7, 8]
-})
+    query = st.text_input('Enter your sql query')
 
-st.write('Please note that table name is df')
-st.write('The entered query is:', query)
+    st.write('The beverages table is:')
+    st.dataframe(beverages)
 
-st.write('The initial dataframe is:')
-st.dataframe(df)
+    st.write('The food_items table is:')
+    st.dataframe(food_items)
 
+    if query:
+        st.write('The entered query is:', query)
+        result = duckdb.query(query).to_df()
+        st.write('The queried dataframe is:', result)
 
-queried_df = duckdb.query(query).to_df()
+    st.write("The expected result is:", solution)
 
-st.write('The queried dataframe is:', queried_df)
-
-
-
+with tab2:
+    st.write("""
+    # SQL SRS
+    Spaced Repetition System SQL practice
+    """)
+    st.write('The solution is:', answer)
+    st.dataframe(solution)
