7627e61e5b259d4f29de4e697e1448ad00b87992
Arnaud Mouttapa <arnaudmouttapa@gmail.com>
2024-06-18 13:48:28 +0200

    add solution file and modify app.py to get solution from specific file

--- app.py
+++ app.py
@@ -14,20 +14,28 @@ with st.sidebar:
         placeholder="Select a theme",
     )
     if option:
-        exercise = conn.execute(f"SELECT * FROM memory_state WHERE theme = '{option}'").df()
-        st.write(exercise)
+        exercises = conn.execute(f"SELECT * FROM memory_state WHERE theme = '{option}'").df()
+        st.write(exercises)
+        exercise_name = exercises["exercise_name"][0]
+        with open(f"solutions/{exercise_name}.sql", "r") as file:
+            solution_query = file.read()
+        solution_df = conn.execute(solution_query).df()
 
-tab1, tab2 = st.tabs(["Tables", "Solution"])
-
-with tab1:
-    st.write(
+st.write(
         """
     # SQL SRS
     Spaced Repetition System SQL practice
     """
     )
+
+query = st.text_input("Enter your sql query")
+
+tab1, tab2 = st.tabs(["Tables", "Solution"])
+
+with tab1:
+    
     if option:
-        for table in exercise["tables"][0]:
+        for table in exercises["tables"][0]:
             df = conn.execute(f"SELECT * FROM {table}").df()
             st.write(f"The {table} table is:")
             st.dataframe(df)
@@ -35,20 +43,30 @@ with tab1:
     else:
         st.write("Choose a theme to review")
     
-    query = st.text_input("Enter your sql query")
+    
 
     if query:
         result = conn.execute(query).df()
         st.write("The queried dataframe is:", result)
 
-    # st.write("The expected output is:", solution)
+        if result.shape[0] != solution_df.shape[0]:
+            st.write("The number of rows is incorrect")
+        elif result.shape[1] != solution_df.shape[1]:
+            st.write("The number of columns is incorrect")
+        elif not result.compare(solution_df).empty:
+            st.write("The content is incorrect")
+        else:
+            st.write("The query is correct !")
 
-# with tab2:
-#     st.write(
-#         """
-#     # SQL SRS
-#     Spaced Repetition System SQL practice
-#     """
-#     )
-#     st.write("The solution is:", answer)
-#     st.dataframe(solution)
+    
+    if option:
+        st.write("The expected output is:")
+        st.dataframe(solution_df)
+
+with tab2:
+    if option:
+        if query:
+            result = conn.execute(query).df()
+            st.write("The queried dataframe is:", result)
+        st.write("The solution is:", solution_query)
+        st.dataframe(solution_df)
