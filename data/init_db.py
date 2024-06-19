bbcd45ab2d02f0be147f26266af2089b9cf168ea
Arnaud Mouttapa <arnaudmouttapa@gmail.com>
2024-06-19 22:19:17 +0200

    add sizes and trademarks cross join exercise

--- data/init_db.py
+++ data/init_db.py
@@ -10,13 +10,15 @@ conn = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=Fal
 # ------------------------------------------------------------------
 
 data = {
-    "theme": ["Cross Joins", "Window Functions"],
-    "exercise_name": ["beverages_and_food", "simple_window"],
-    "tables": [["beverages", "food_items"], ["simple_window"]],
-    "last_reviewed": ["2021-09-01", "2021-09-01"],
+    "theme": ["Cross Joins", "Cross Joins"],
+    "exercise_name": ["beverages_and_food", "sizes_and_trademarks"],
+    "tables": [["beverages", "food_items"], ["sizes", "trademark"]],
+    "last_reviewed": ["2022-09-01", "2021-09-01"],
 }
 memory_state_df = pd.DataFrame(data)
-conn.execute("CREATE TABLE IF NOT EXISTS memory_state AS SELECT * FROM memory_state_df")
+conn.execute(
+    "CREATE OR REPLACE TABLE memory_state AS SELECT * FROM memory_state_df ORDER by last_reviewed"
+)
 
 # ------------------------------------------------------------------
 # CROSS JOIN EXERCISES
@@ -40,3 +42,26 @@ muffin,3
 
 food_items = pd.read_csv(io.StringIO(csv2))
 conn.execute("CREATE TABLE IF NOT EXISTS food_items AS SELECT * FROM food_items")
+
+csv3 = """
+size
+XS
+S
+M
+L
+XL
+"""
+
+sizes = pd.read_csv(io.StringIO(csv3))
+conn.execute("CREATE TABLE IF NOT EXISTS sizes AS SELECT * FROM sizes")
+
+csv4 = """
+trademark
+Nike
+Asphalte
+Abercrombie
+Levis
+"""
+
+trademarks = pd.read_csv(io.StringIO(csv4))
+conn.execute("CREATE TABLE IF NOT EXISTS trademark AS SELECT * FROM trademark")
